4b68fa0036c0d417df9cd42453f7a4f52d22f21a
ivanextrastuff <19ileung@cms.tela.org.uk>
2025-04-03 14:27:31 +0100

    modifying procfile

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -32,5 +33,7 @@ async def get_stock(symbol: str):
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+app.mount("/", StaticFiles(directory="build", html=True))
 
 # Run with 'uvicorn server:app --reload'
