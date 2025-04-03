e786b0c2ea94a80b635fd75c5bb31e0d63bb51e7
IvanLeung36 <ivanleung36@gmail.com>
2025-04-03 11:47:45 +0100

    removing useRef

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { getStockData } from "../services/stockService";
 import { StockGraph } from "../components/StockGraph";
 import "./Search.css";
